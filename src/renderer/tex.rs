1957c650ccf394d952bb76f99d1e01ee20678240
Sven Schober <sv3sch@gmail.com>
2023-02-17 20:50:06 +0100

    clippy - tex findings

--- src/renderer/tex.rs
+++ src/renderer/tex.rs
@@ -8,7 +8,7 @@ pub struct TexRenderer;
 impl Renderer for TexRenderer {
     fn render(&self, exp: Exp) -> String {
         match exp {
-            Exp::Literal(s) => format!("{}", s),
+            Exp::Literal(s) => s,
             Exp::EscapeLit(s) => format!("\\{}",s),
             Exp::Bold(b_exp) => format!("\\textbf{{{}}}", self.render(*b_exp)),
             Exp::Italic(b_exp) => format!("\\textit{{{}}}", self.render(*b_exp)),
@@ -25,7 +25,7 @@ impl Renderer for TexRenderer {
             Exp::Footnote(b_exp) => format!("~\\footnote{{{}}}", self.render(*b_exp)),
             Exp::HyperRef(b_exp1, b_exp2) => format!("\\href{{{}}}{{{}}}", self.render(*b_exp2), self.render(*b_exp1)),
             Exp::Cat(b_exp1, b_exp2) => format!("{}{}", self.render(*b_exp1), self.render(*b_exp2)),
-            Exp::Empty() => format!(""),
+            Exp::Empty() => String::new(),
         }
     }
 
