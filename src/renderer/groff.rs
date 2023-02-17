be6bff244fb612575625bb98ee5cd32378e4c296
Sven Schober <sv3sch@gmail.com>
2023-02-17 20:49:55 +0100

    clippy - groff findings

--- src/renderer/groff.rs
+++ src/renderer/groff.rs
@@ -7,11 +7,11 @@ pub struct GroffRenderer;
 impl Renderer for GroffRenderer {
     fn render(&self, exp: Exp) -> String {
         match exp {
-            Exp::Literal(s) => format!("{}", s),
+            Exp::Literal(s) => s,
             Exp::EscapeLit(s) => {
                 match s.as_str() {
-                    "." => format!("\\&."),
-                    _ => format!("{}",s)
+                    "." => "\\&.".to_string(),
+                    _ => s
                 }
             },
             Exp::Bold(b_exp) => format!("\\*[BOLDER]{}\\*[BOLDERX]", self.render(*b_exp)),
@@ -24,7 +24,7 @@ impl Renderer for GroffRenderer {
             Exp::Footnote(b_exp) => format!("\n.FOOTNOTE\n{}\n.FOOTNOTE END\n", self.render(*b_exp)),
             Exp::HyperRef(b_exp1, b_exp2) => format!(".PDF_WWW_LINK {} \"{}\"", self.render(*b_exp2), self.render(*b_exp1)),
             Exp::Cat(b_exp1, b_exp2) => format!("{}{}", self.render(*b_exp1), self.render(*b_exp2)),
-            Exp::Empty() => format!(""),
+            Exp::Empty() => String::new(),
         }
     }
 
