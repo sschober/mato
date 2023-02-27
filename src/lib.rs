2b83e7a095551c253418033085c17607e019f636
Sven Schober <sv3sch@gmail.com>
2023-02-27 18:05:07 +0100

    some comments on Exp

--- src/lib.rs
+++ src/lib.rs
@@ -15,15 +15,21 @@ fn render<T: renderer::Renderer>(t: T, exp: Exp) -> String {
 /// Expressions are the building blocks of an abstract syntax tree
 #[derive(Debug)]
 pub enum Exp {
+    /// A literal is a string rendered as is
     Literal(String),
+    /// An escaped literal probabely has to be treated in a special way, depending on the rendering backend
+    EscapeLit(String),
+    /// Section headers with a separate parameter specifying the level
     Heading(Box<Exp>, u8),
+    /// Encapsulates boldness; can contain various other formattings 
     Bold(Box<Exp>),
+    /// Encapsulates cursiveness; can contain varios other formattings
     Italic(Box<Exp>),
+    /// Encapsulates text rendered in non-proportional font, usually used for computer code
     Teletype(Box<Exp>),
     Quote(Box<Exp>),
     Footnote(Box<Exp>),
     HyperRef(Box<Exp>, Box<Exp>),
-    EscapeLit(String),
     // this enables composition, forming the tree
     Cat(Box<Exp>, Box<Exp>),
     // this is a neutral element, yielding no ouput
