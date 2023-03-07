0647f3ddcaf5b9897a5ff7efc160043c25211987
Sven Schober <sv3sch@gmail.com>
2023-03-07 23:04:26 +0100

    enhance groff renderer with true nested font styles

--- src/renderer/groff.rs
+++ src/renderer/groff.rs
@@ -6,8 +6,12 @@ use super::Renderer;
 /// empty struct to attach Renderer implementation on
 pub struct GroffRenderer;
 
-impl Renderer for GroffRenderer {
-    fn render(&self, exp: Exp) -> String {
+impl GroffRenderer {
+    /// groff does not support nested formattings, because it has no
+    /// stackable way of switching back to the previous style. we
+    /// need to emulate this by passing in the parent style as a
+    /// parameter, parent_format.
+    fn render_with_parent_format(&self, exp: Exp, parent_format: &str) -> String {
         match exp {
             Exp::Literal(s) => s,
             Exp::EscapeLit(s) => {
@@ -16,8 +20,16 @@ impl Renderer for GroffRenderer {
                     _ => s
                 }
             },
-            Exp::Bold(b_exp) => format!("\\*[BOLDER]{}\\*[BOLDERX]", self.render(*b_exp)),
-            Exp::Italic(b_exp) => format!("\\*[SLANT]{}\\*[SLANTX]", self.render(*b_exp)),
+            Exp::Bold(b_exp) => {
+                format!("\\*[BD]{}\\*[{}]",
+                        self.render_with_parent_format(*b_exp, "BD"),
+                        parent_format)
+            },
+            Exp::Italic(b_exp) => {
+                format!("\\*[IT]{}\\*[{}]",
+                        self.render_with_parent_format(*b_exp, "IT"),
+                        parent_format)
+            },
             Exp::CodeBlock(b_exp) => format!(".QUOTE_STYLE INDENT 1\n.QUOTE\n.CODE\n.BOX OUTLINED black INSET 18p\n{}.BOX OFF\n.QUOTE OFF", self.render(*b_exp)),
             Exp::InlineCode(b_exp) => format!("\\*[CODE]{}\\*[CODE OFF]", self.render(*b_exp)),
             Exp::Heading(b_exp, level) => {
@@ -26,9 +38,18 @@ impl Renderer for GroffRenderer {
             Exp::Quote(b_exp) => format!("\"{}\"", self.render(*b_exp)),
             Exp::Footnote(b_exp) => format!("\n.FOOTNOTE\n{}\n.FOOTNOTE END\n", self.render(*b_exp)),
             Exp::HyperRef(b_exp1, b_exp2) => format!(".PDF_WWW_LINK {} \"{}\"", self.render(*b_exp2), self.render(*b_exp1)),
-            Exp::Cat(b_exp1, b_exp2) => format!("{}{}", self.render(*b_exp1), self.render(*b_exp2)),
+            Exp::Cat(b_exp1, b_exp2) => {
+                format!("{}{}",
+                        self.render_with_parent_format(*b_exp1, parent_format),
+                        self.render_with_parent_format(*b_exp2, parent_format))
+            },
             Exp::Empty() => String::new(),
         }
     }
+}
 
+impl Renderer for GroffRenderer {
+    fn render(&self, exp: Exp) -> String {
+        self.render_with_parent_format(exp, "ROM")
+    }
 }
