938cc668e5c5a95f9d17064ca354a58f47306758
Sven Schober <sv3sch@gmail.com>
2023-08-21 08:08:30 +0200

    recursively descent into MetaDataBlock during canonicalize
    closes #27

--- mato/src/process/canonicalize.rs
+++ mato/src/process/canonicalize.rs
@@ -1,6 +1,7 @@
 use super::Process;
 
 use crate::Exp;
+use crate::syntax::meta_data_block;
 
 pub struct Canonicalizer {}
 
@@ -8,9 +9,10 @@ pub struct Canonicalizer {}
 fn erase_empty(exp: Exp) -> Exp {
     match exp {
         Exp::Cat(b_exp1, b_exp2) => match *b_exp1 {
-            Exp::Empty() => *b_exp2,
+            Exp::Empty() => erase_empty(*b_exp2),
             _ => erase_empty(*b_exp1).cat(erase_empty(*b_exp2)),
         },
+        Exp::MetaDataBlock(b_exp) => meta_data_block(erase_empty(*b_exp)),
         _ => exp,
     }
 }
