6d58777f014c0572a6c142e5d05a73e167e8eea8
Sven Schober <sv3sch@gmail.com>
2023-11-24 17:54:51 +0100

    introduce abbreviating macros in groff renderer

--- mato/src/render/groff.rs
+++ mato/src/render/groff.rs
@@ -10,12 +10,6 @@ pub struct Renderer {
     document_started: bool,
 }
 
-impl Default for Renderer {
-    fn default() -> Self {
-        new()
-    }
-}
-
 pub fn new() -> Renderer {
     Renderer {
         ctx: HashMap::new(),
@@ -33,6 +27,20 @@ impl Renderer {
     /// need to emulate this by passing in the parent style as a
     /// parameter, `parent_format`.
     fn render_with_parent_format(&mut self, exp: Exp, parent_format: &str) -> String {
+        // abbreviates call to self.render_with_default_format(...)
+        macro_rules! rnd {
+            ($( $args:expr ), *) => {
+                self.render_with_default_format($( $args ),*)
+            }
+        }
+
+        // abbreviates call to self.render_with_parent_format(...)
+        macro_rules! rnd_pf {
+            ($( $args:expr ), *) => {
+                self.render_with_parent_format($( $args ),*)
+            }
+        }
+
         match exp {
             Exp::Document() => {
                 // eprintln!("{:?}", self.ctx);
@@ -87,72 +95,47 @@ impl Renderer {
                 _ => s,
             },
             Exp::Bold(b_exp) => {
-                format!(
-                    "\\*[BD]{}\\*[{}]",
-                    self.render_with_parent_format(*b_exp, "BD"),
-                    parent_format
-                )
+                format!("\\*[BD]{}\\*[{}]", rnd_pf!(*b_exp, "BD"), parent_format)
             }
             Exp::Italic(b_exp) => {
-                format!(
-                    "\\*[IT]{}\\*[{}]",
-                    self.render_with_parent_format(*b_exp, "IT"),
-                    parent_format
-                )
+                format!("\\*[IT]{}\\*[{}]", rnd_pf!(*b_exp, "IT"), parent_format)
             }
             // Currently there seems to be a bug: https://savannah.gnu.org/bugs/index.php?64561
             // Exp::CodeBlock(b_exp) => format!(".QUOTE_STYLE INDENT 1\n.QUOTE\n.CODE\n.BOX OUTLINED black INSET 18p\n{}.BOX OFF\n.QUOTE OFF", self.render(*b_exp)),
             Exp::CodeBlock(_b1, b2) => format!(
                 ".QUOTE_STYLE INDENT 1\n.QUOTE\n.CODE\n{}.QUOTE OFF",
-                self.render_with_default_format(*b2)
+                rnd!(*b2)
             ),
-            Exp::InlineCode(b_exp) => format!(
-                "\\*[CODE]{}\\*[CODE OFF]",
-                self.render_with_default_format(*b_exp)
-            ),
+            Exp::InlineCode(b_exp) => format!("\\*[CODE]{}\\*[CODE OFF]", rnd!(*b_exp)),
             Exp::Heading(b_exp, level) => {
                 if self.is_doctype("CHAPTER") {
                     if level == 0 {
                         if self.document_started {
-                            format!(
-                                ".COLLATE\n.CHAPTER_TITLE \"{}\"\n.START\n",
-                                self.render_with_default_format(*b_exp)
-                            )
+                            format!(".COLLATE\n.CHAPTER_TITLE \"{}\"\n.START\n", rnd!(*b_exp))
                         } else {
                             self.document_started = true;
-                            format!(
-                                ".CHAPTER_TITLE \"{}\"\n.START\n",
-                                self.render_with_default_format(*b_exp)
-                            )
+                            format!(".CHAPTER_TITLE \"{}\"\n.START\n", rnd!(*b_exp))
                         }
                     } else {
                         format!(
                             ".SPACE -.7v\n.EW 2\n.HEADING {} \"{}\"\n.EW 0\n",
                             level + 2,
-                            self.render_with_default_format(*b_exp)
+                            rnd!(*b_exp)
                         )
                     }
                 } else if self.is_doctype("SLIDES") {
                     if level == 0 {
                         if self.document_started {
-                            format!(
-                                ".NEWSLIDE\n.HEADING {} \"{}\"\n",
-                                level + 1,
-                                self.render_with_default_format(*b_exp)
-                            )
+                            format!(".NEWSLIDE\n.HEADING {} \"{}\"\n", level + 1, rnd!(*b_exp))
                         } else {
                             self.document_started = true;
-                            format!(
-                                ".START\n.HEADING {} \"{}\"\n",
-                                level + 1,
-                                self.render_with_default_format(*b_exp)
-                            )
+                            format!(".START\n.HEADING {} \"{}\"\n", level + 1, rnd!(*b_exp))
                         }
                     } else {
                         format!(
                             ".SPACE -.7v\n.EW 2\n.HEADING {} \"{}\"\n.EW 0\n",
                             level + 1,
-                            self.render_with_default_format(*b_exp)
+                            rnd!(*b_exp)
                         )
                     }
                 } else {
@@ -160,25 +143,25 @@ impl Renderer {
                     if 3 == level {
                         format!(
                             ".SPACE -1v\n.MN LEFT\n\\!.ALD 1v\n{}\n.MN OFF",
-                            self.render_with_default_format(*b_exp)
+                            rnd!(*b_exp)
                         )
                     } else if 0 == level {
                         format!(
                             ".SPACE -.7v\n.FT B\n.EW 2\n.HEADING {} \"{}\"\n.EW 0\n.FT R\n.DRH\n",
                             level + 1,
-                            &self.render_with_default_format(*b_exp)
+                            &rnd!(*b_exp)
                         )
                     } else if 1 == level {
                         format!(
                             ".SPACE -.7v\n.FT B\n.EW 2\n.HEADING {} \"{}\"\n.EW 0\n.FT R\n",
                             level + 1,
-                            &self.render_with_default_format(*b_exp)
+                            &rnd!(*b_exp)
                         )
                     } else {
                         format!(
                             ".SPACE -.7v\n.EW 2\n.HEADING {} \"{}\"\n.EW 0\n",
                             level + 1,
-                            self.render_with_default_format(*b_exp)
+                            rnd!(*b_exp)
                         )
                     }
                 }
@@ -186,52 +169,41 @@ impl Renderer {
             Exp::ChapterMark(b_exp) => {
                 format!(
                     ".MN RIGHT\n.PT_SIZE +48\n.COLOR grey\n{}\n.MN OFF\n",
-                    self.render_with_default_format(*b_exp)
+                    rnd!(*b_exp)
                 )
             }
             Exp::RightSidenote(b_exp) => {
-                format!(
-                    "\n.MN RIGHT\n.PT_SIZE -2\n{}\n.MN OFF\n",
-                    self.render_with_default_format(*b_exp)
-                )
+                format!("\n.MN RIGHT\n.PT_SIZE -2\n{}\n.MN OFF\n", rnd!(*b_exp))
             }
-            Exp::Quote(b_exp) => format!("\"{}\"", self.render_with_default_format(*b_exp)),
+            Exp::Quote(b_exp) => format!("\"{}\"", rnd!(*b_exp)),
             Exp::Footnote(b_exp) => {
-                format!(
-                    "\n.FOOTNOTE\n{}\n.FOOTNOTE END\n",
-                    self.render_with_default_format(*b_exp)
-                )
+                format!("\n.FOOTNOTE\n{}\n.FOOTNOTE END\n", rnd!(*b_exp))
             }
-            Exp::HyperRef(b_exp1, b_exp2) => format!(
-                ".PDF_WWW_LINK {} \"{}\"",
-                self.render_with_default_format(*b_exp2),
-                self.render_with_default_format(*b_exp1)
-            ),
+            Exp::HyperRef(b_exp1, b_exp2) => {
+                format!(".PDF_WWW_LINK {} \"{}\"", rnd!(*b_exp2), rnd!(*b_exp1))
+            }
             Exp::Cat(b_exp1, b_exp2) => {
                 format!(
                     "{}{}",
-                    self.render_with_parent_format(*b_exp1, parent_format),
-                    self.render_with_parent_format(*b_exp2, parent_format)
+                    rnd_pf!(*b_exp1, parent_format),
+                    rnd_pf!(*b_exp2, parent_format)
                 )
             }
             Exp::Empty() => String::new(),
             Exp::List(b_exp, _) => {
-                format!(
-                    ".LIST\n.SHIFT_LIST 18p\n{}.LIST OFF\n",
-                    self.render_with_default_format(*b_exp)
-                )
+                format!(".LIST\n.SHIFT_LIST 18p\n{}.LIST OFF\n", rnd!(*b_exp))
             }
             Exp::ListItem(b_exp, _) => match *b_exp {
                 Exp::Empty() => String::new(),
-                _ => format!(".ITEM\n{}\n", self.render_with_default_format(*b_exp)),
+                _ => format!(".ITEM\n{}\n", rnd!(*b_exp)),
             },
-            Exp::MetaDataBlock(b_exp) => self.render_with_default_format(*b_exp),
+            Exp::MetaDataBlock(b_exp) => rnd!(*b_exp),
             Exp::MetaDataItem(_, _) => String::new(),
             Exp::Image(b_exp, path) => {
                 format!(
                     ".PDF_IMAGE {} 200p 150p CAPTION \"{}\"",
-                    self.render_with_default_format(*path),
-                    self.render_with_default_format(*b_exp)
+                    rnd!(*path),
+                    rnd!(*b_exp)
                 )
             }
         }
