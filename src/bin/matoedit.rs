2a4479ebdbabfa7aa79ac42652d9506b09b5ec14
Sven Schober <sv3sch@gmail.com>
2023-12-27 22:23:59 +0100

    document wezterm_cli

--- src/bin/matoedit.rs
+++ src/bin/matoedit.rs
@@ -1,6 +1,12 @@
 use mato::wezterm_cli;
 use std::{env, thread, time};
 
+/// spawns a new wezterm pane in a new tab and opens the
+/// passed in file in and editor in said pane.
+/// then splits the pane and launches `matopdf` on the file.
+/// then waits for a sec to let `matopdf` finish, then 
+/// proceeds to create a toplevel split pane to the right,
+/// wehere `termpdf.py` is launched on the resulting pdf.
 fn main() -> std::io::Result<()> {
     let args: Vec<String> = env::args().collect();
     if args.len() != 2 {
@@ -17,15 +23,18 @@ fn main() -> std::io::Result<()> {
         Err(_) => "nvim".to_string(),
     };
 
+    // SPAWN the EDITOR pane!
     let editor_pane = wezterm_cli::spawn(&format!("{} {}", editor_cmd, source_file));
     eprintln!("editor pane id: {}", editor_pane.id);
 
+    // SPLIT the pane and LAUNCH matopdf
     let mato_pane = editor_pane.split(
         vec!["--percent", "10", "--bottom"],
         format!("matopdf -w -v {}", source_file).as_str(),
     );
     eprintln!("mato pane id: {}", mato_pane.id);
 
+    // WAIT a sec
     // this is ugly, but we need to sleep 1 sec to give
     // matopdf time to transform the source file,
     // otherwise termpdf would bail out
@@ -35,13 +44,16 @@ fn main() -> std::io::Result<()> {
     let target_file_path = mato::replace_file_extension(source_file, "pdf");
     eprintln!("target file: {}", target_file_path.display());
 
+    // SPLIT the pand toplevel and LAUNCH `termpdf.py``
     let termpdf_pane = editor_pane.split(
         vec!["--top-level", "--right"],
         &format!("termpdf.py {}", target_file_path.display()),
     );
     eprintln!("termpdf pane id: {}", termpdf_pane.id);
 
-    // split and spawn move focus to the newly created panes, so we need to refocus on the editor
+    // FOCUS the EDITOR
+    // split and spawn move focus to the newly created panes, 
+    // so we need to refocus on the editor
     editor_pane.activate();
 
     Ok(())
--- src/wezterm_cli.rs
+++ src/wezterm_cli.rs
@@ -1,5 +1,6 @@
 use std::{env, process::Command};
 
+/// acquires and returns the current directory as a `String``
 fn current_dir() -> String {
     env::current_dir()
         .unwrap()
@@ -9,6 +10,8 @@ fn current_dir() -> String {
         .to_string()
 }
 
+/// executes the given `cmd` as a sub process and 
+/// returns its output as a string
 fn exec(cmd: Vec<&str>) -> String {
     eprintln!("exec: {:?}", cmd);
     let out = Command::new("/usr/bin/env")
@@ -26,15 +29,30 @@ fn exec(cmd: Vec<&str>) -> String {
         String::new()
     }
 }
+
+/// returns a vector with `wezterm` and `cli` as members
 fn wezterm_cli_vec() -> Vec<&'static str> {
     vec!["wezterm", "cli"]
 }
+
+/// returns a vector with `zsh`, `-c` and `cmd` as constituents
+/// effectively wrapping a command in a shell
 fn zsh_c_vec(cmd: &str) -> Vec<&str> {
     vec!["zsh", "-c", cmd]
 }
+
+/// returns a vector with the `--cwd`` option as first element 
+/// and the passed in `dir` string as second 
 fn current_dir_vec(dir: &str) -> Vec<&str> {
     vec!["--cwd", dir]
 }
+
+/// Spans a new pane, setting CWD to the current directory 
+/// (otherwise it would be set to $HOME).
+/// The passed in command is wrapped in a zsh invocation.
+/// This is necessary, as otherwise, the environment and 
+/// more specifically the $PATH variable would lack the 
+/// user's settings.
 pub fn spawn(cmd: &str) -> Pane {
     let pane_id = exec(
         [
@@ -53,10 +71,17 @@ pub fn spawn(cmd: &str) -> Pane {
 pub struct Pane {
     pub id: String,
 }
+
 impl Pane {
+    /// returns a vector with `--pane-id` and the 
+    /// pane id as members
     fn pane_id_vec(&self) -> Vec<&str> {
         vec!["--pane-id", self.id.as_str()]
     }
+    /// splits the current pane and launches `cmd`.
+    /// the passed in vector of `opts` allows for customization:
+    /// how big the new split is supposed to be and where should 
+    /// it be located. 
     pub fn split(&self, opts: Vec<&str>, cmd: &str) -> Pane {
         let pane_id = exec(
             [
@@ -71,6 +96,7 @@ impl Pane {
         );
         Pane { id: pane_id }
     }
+    /// activates the pane identified by `self`, which means, it gets the focus 
     pub fn activate(&self) {
         exec([wezterm_cli_vec(), vec!["activate-pane"], self.pane_id_vec()].concat());
     }
