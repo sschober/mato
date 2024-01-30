58f7ff1cfede1cb531b3e7c184ba82f3cf292ef3
Sven Schober <sv3sch@gmail.com>
2024-01-30 12:18:53 +0100

    fix asymmetrical behaviour for code samples

--- samples/groff/simple/codeblock.md
+++ samples/groff/simple/codeblock.md
@@ -1,6 +1,13 @@
 Some line before:
-``` 
+```
 this is a code block
-``` 
-
+```
 Some line after
+
+Further paragraph:
+
+```
+A new code block with a free line around.
+```
+
+and after.
--- src/render/groff.rs
+++ src/render/groff.rs
@@ -103,7 +103,7 @@ impl Renderer {
             // Currently there seems to be a bug: https://savannah.gnu.org/bugs/index.php?64561
             // Exp::CodeBlock(b_exp) => format!(".QUOTE_STYLE INDENT 1\n.QUOTE\n.CODE\n.BOX OUTLINED black INSET 18p\n{}.BOX OFF\n.QUOTE OFF", self.render(*b_exp)),
             Exp::CodeBlock(_b1, b2) => format!(
-                ".QUOTE_STYLE INDENT 1\n.QUOTE\n.CODE\n{}.QUOTE OFF",
+                ".QUOTE_STYLE INDENT 1\n.QUOTE\n.CODE\n{}.QUOTE OFF\n",
                 rnd!(*b2)
             ),
             Exp::InlineCode(b_exp) => format!("\\*[CODE]{}\\*[CODE OFF]", rnd!(*b_exp)),
