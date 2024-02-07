4de4f195248e470b2ed98917f7492c2a59cdf892
Sven Schober <sv3sch@gmail.com>
2024-02-07 15:13:01 +0100

    fix bug where caret character is not rendered in code blocks

--- src/process/canonicalize.rs
+++ src/process/canonicalize.rs
@@ -3,7 +3,7 @@ use std::collections::HashMap;
 use super::Process;
 
 use crate::config::Config;
-use crate::syntax::meta_data_block;
+use crate::syntax::{meta_data_block, prelit};
 use crate::{log_trc, Exp};
 
 /// The Canonicalizer processor removes unneeded AST
@@ -20,8 +20,13 @@ fn erase_empty(exp: Exp) -> Exp {
         Exp::CodeBlock(b1, b2) => Exp::CodeBlock(b1, Box::new(erase_empty(*b2))),
         Exp::MetaDataBlock(b_exp) => meta_data_block(erase_empty(*b_exp)),
         Exp::ChapterMark(b_exp) => Exp::ChapterMark(Box::new(erase_empty(*b_exp))),
+        Exp::PreformattedLiteral(s) => prelit(&escape_groff_symbols(s)),
         _ => exp,
     }
+}
+
+fn escape_groff_symbols(s: String) -> String {
+    s.replace("^", "\\[ha]")
 }
 
 impl Process for Canonicalizer {
