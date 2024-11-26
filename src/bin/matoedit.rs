b18baf744f9194a3a7af524e1cc06b2949b7f5b6
Sven Schober <sv3sch@gmail.com>
2024-11-26 16:49:15 +0100

    opts: improve adding of options
    now, there is a macro to add options.

--- src/bin/matoedit.rs
+++ src/bin/matoedit.rs
@@ -1,32 +1,9 @@
 use std::env;
 
-use mato::opts::Opt;
 use mato::term_cli::TermCli;
-use mato::{opts, print_version};
+use mato::{opt_val, opts, print_version};
 
-#[derive(Default)]
-struct Config {
-    source_file: String,
-    lang: String,
-}
-
-fn parse_config() -> Option<Config> {
-    let args: Vec<String> = env::args().collect();
-    if args.len() < 2 {
-        None
-    } else {
-        let mut result = Config::default();
-        for arg in args {
-            match arg.as_str() {
-                "-len" | "-l en" => result.lang = "en".to_owned(),
-                _ => result.source_file = arg,
-            }
-        }
-        Some(result)
-    }
-}
-
-const VERSION: &str = "0.1.0";
+const VERSION: &str = "0.1.1";
 const PROG_NAME: &str = "matoedit";
 
 /// spawns a new wezterm pane in a new tab and opens the
@@ -37,52 +14,52 @@ const PROG_NAME: &str = "matoedit";
 /// wehere `termpdf.py` is launched on the resulting pdf.
 fn main() -> std::io::Result<()> {
     let mut p = opts::Parser::new();
-    p.add_opt(Opt::Flag {
-        short_name: "v".to_owned(),
-        long_name: "version".to_owned(),
-        description: "Print command version".to_owned(),
-    });
-    p.add_opt(Opt::Value {
-        short_name: "s".to_owned(),
-        long_name: "source-file".to_owned(),
-        description: "Source file".to_owned(),
-    });
+    p.add_opt(opt_val!("l", "lang", "Language of source file.", "de"));
 
     let parsed_opts = p.parse(env::args().collect());
-
-    if parsed_opts.contains_key("version") {
+    // eprintln!("{:?}", parsed_opts);
+    if parsed_opts.opts.contains_key("version") {
         print_version(PROG_NAME, VERSION);
         return Ok(());
     }
 
-    // ACQUIRE cli handle, panics if not supported
-    let term_cli = TermCli::get();
+    let lang = parsed_opts.get_opt("lang");
+    // eprintln!("language: {}", lang);
 
-    let args: Vec<String> = env::args().collect();
-    if args.len() != 2 {
+    if parsed_opts.opts.contains_key("help") {
+        print_version(PROG_NAME, VERSION);
+        p.print_usage_string();
+        return Ok(());
+    }
+
+    let mut args: Vec<String> = parsed_opts.params.clone();
+    if args.len() != 1 {
         panic!("need a file as argument!");
     }
 
-    let config = parse_config().expect("need source file");
-    eprintln!("file to open: {}", config.source_file);
+    let source_file = args.pop().unwrap();
+    eprintln!("source file: {}", source_file);
+
+    // ACQUIRE cli handle, panics if not supported
+    let term_cli = TermCli::get();
 
     // we create the source file in any case, so that we can
     // immediately transform it.
-    mato::create_if_not_exists(&config.source_file);
+    mato::create_if_not_exists(&source_file);
 
     // OPEN editor
     let editor_handle = term_cli.get_active_windows_handle();
     eprintln!("editor handle: {}", editor_handle);
 
     // we need to figure out the target file name for termpdf to call on
-    let target_file_path = mato::replace_file_extension(&config.source_file, "pdf");
+    let target_file_path = mato::replace_file_extension(&source_file, "pdf");
     eprintln!("target file: {}", target_file_path.display());
 
     // CREATE empty pdf if none is there already
     mato::create_empty_if_not_exists(&format!("{}", target_file_path.display()));
 
     // LAUNCH matopdf
-    let mato_handle = term_cli.exec_matopdf(&config.source_file, &config.lang, editor_handle);
+    let mato_handle = term_cli.exec_matopdf(&source_file, &lang, editor_handle);
     eprintln!("mato handle: {}", mato_handle);
 
     // LAUNCH `termpdf.py`
@@ -96,7 +73,7 @@ fn main() -> std::io::Result<()> {
     term_cli.focus(editor_handle);
 
     // OPEN editor and block on call
-    term_cli.open_editor(&config.source_file);
+    term_cli.open_editor(&source_file);
 
     // CLOSE evrything
     term_cli.close(mato_handle);
--- src/opts.rs
+++ src/opts.rs
@@ -1,7 +1,7 @@
 use std::collections::HashMap;
 /// We do our option parsing ourselves, NIH (not inventied here) syndrome.
 
-#[derive(Clone)]
+#[derive(Clone, Debug)]
 pub enum Opt {
     Flag {
         short_name: String,
@@ -12,6 +12,7 @@ pub enum Opt {
         short_name: String,
         long_name: String,
         description: String,
+        default: String,
     },
 }
 
@@ -21,13 +22,84 @@ pub struct Parser {
     pub short_opts: HashMap<String, Opt>,
 }
 
+/// ParserResult captures the parsed entites extracted
+/// from the command line arguments.
+#[derive(Debug)]
+pub struct ParserResult {
+    pub command_name: String,
+    /// Maps long name key to optional value: `key = value`.
+    /// value is empty string if option was a flag.
+    pub opts: HashMap<String, String>,
+    /// arguments on the command line, that were not options
+    pub params: Vec<String>,
+    long_opts: HashMap<String, Opt>,
+}
+impl ParserResult {
+    /// gets option value. guaranteed to return a value, as
+    /// defaults are part of the definition.
+    pub fn get_opt(&self, key: &str) -> String {
+        // TODO this is not so easy, because if a key is not
+        // present in self.opts, we need to look it up in the
+        // given opts of the parser, which we do not have here
+        // in the results
+        self.opts
+            .get(key)
+            .cloned()
+            .unwrap_or_else(|| match self.long_opts.get(key).unwrap() {
+                Opt::Flag {
+                    short_name: _,
+                    long_name: _,
+                    description: _,
+                } => "".to_owned(),
+                Opt::Value {
+                    short_name: _,
+                    long_name: _,
+                    description: _,
+                    default,
+                } => default.clone(),
+            })
+    }
+}
+
+#[macro_export]
+macro_rules! opt_val {
+    ($sn:tt, $ln:tt, $ds:tt, $de:tt) => {
+        crate::opts::Opt::Value {
+            short_name: $sn.to_owned(),
+            long_name: $ln.to_owned(),
+            description: $ds.to_owned(),
+            default: $de.to_owned(),
+        }
+    };
+}
+
+#[macro_export]
+macro_rules! opt_flag {
+    ($sn:tt, $ln:tt, $ds:tt) => {
+        crate::opts::Opt::Flag {
+            short_name: $sn.to_owned(),
+            long_name: $ln.to_owned(),
+            description: $ds.to_owned(),
+        }
+    };
+}
+
 impl Parser {
     pub fn new() -> Self {
-        Parser {
+        let mut p = Parser {
             long_opts: HashMap::new(),
             short_opts: HashMap::new(),
-        }
+        };
+        // add standard flags for help and version
+        p.add_opt(opt_flag!("h", "help", "Print command usage and exit"));
+        p.add_opt(opt_flag!("v", "version", "Print command version and exit"));
+        p
     }
+
+    /// registrs given option to this parser
+    ///
+    /// returns a references to the parser, so calls can be
+    /// chained
     pub fn add_opt(&mut self, opt: Opt) -> &Self {
         match &opt {
             Opt::Flag {
@@ -42,6 +114,7 @@ impl Parser {
                 short_name,
                 long_name,
                 description: _,
+                default: _,
             } => {
                 self.long_opts.insert(long_name.clone(), opt.clone());
                 self.short_opts.insert(short_name.clone(), opt.clone());
@@ -50,52 +123,116 @@ impl Parser {
         self
     }
 
+    /// inserts `key = ""`  (long_name) into `h` for flags, and
+    /// `key = arg+1` for options.
+    ///
+    /// returns the index of element that was added (can be ignored then by caller)
     fn handle_match(
         &self,
         opt: &Opt,
         h: &mut HashMap<String, String>,
         pos: usize,
         args: &Vec<String>,
-    ) {
+    ) -> usize {
+        // different things need to be done, if opt is a
+        // val or a flag
         match opt {
             Opt::Flag {
                 short_name: _,
                 long_name,
                 description: _,
-            } => h.insert(long_name.clone(), "".to_string()),
+            } => {
+                h.insert(long_name.clone(), "".to_string());
+                0
+            }
             Opt::Value {
                 short_name: _,
                 long_name,
                 description: _,
+                default: _,
             } => {
                 if pos + 1 < args.len() {
-                    h.insert(long_name.clone(), args[pos + 1].clone())
+                    h.insert(long_name.clone(), args[pos + 1].clone());
+                    pos + 1
                 } else {
                     panic!("option without value: {}", long_name)
                 }
             }
-        };
+        }
     }
     /// iterates over args and extracts known options,
-    /// returns a HashMap containing all parsed options
-    pub fn parse(&self, args: Vec<String>) -> HashMap<String, String> {
+    /// returns ParserResult containing all parsed options, and
+    /// parameters, that were not options
+    pub fn parse(&self, args: Vec<String>) -> ParserResult {
         let mut h = HashMap::new();
+        let mut p: Vec<String> = Vec::new();
+        let mut c = String::new();
+        let mut skip_pos = 0;
         for (pos, arg) in args.iter().enumerate() {
             let opt_name = arg.trim_start_matches('-');
-            eprintln!("opt name: {}", opt_name);
             if arg.starts_with("--") {
                 match self.long_opts.get(opt_name) {
-                    Some(opt) => self.handle_match(opt, &mut h, pos, &args),
+                    Some(opt) => skip_pos = self.handle_match(opt, &mut h, pos, &args),
                     None => {}
                 }
             } else if arg.starts_with("-") {
                 match self.short_opts.get(opt_name) {
-                    Some(opt) => self.handle_match(opt, &mut h, pos, &args),
+                    Some(opt) => skip_pos = self.handle_match(opt, &mut h, pos, &args),
                     None => {}
+                }
+            } else {
+                // eprintln!("pos: {}, skip_pos: {}", pos, skip_pos);
+                if pos > 0 && pos != skip_pos {
+                    // we skip positional parameter 0, as that is
+                    // the command name
+                    p.push(arg.clone())
+                } else {
+                    // save command name
+                    c = arg.clone()
                 }
             }
         }
-        h
+        ParserResult {
+            command_name: c,
+            opts: h,
+            params: p,
+            long_opts: self.long_opts.clone(),
+        }
+    }
+    pub fn print_usage_string(&self) {
+        // hashmap keys are not sorted, so we sort them
+        // I find this a bit awkward. maybe there is a better way to do that.
+        let mut sorted_keys = self.short_opts.keys().collect::<Vec<&String>>();
+        // why does sort mutate and not return a sorted vec?
+        sorted_keys.sort();
+        for key in sorted_keys {
+            let value = self.short_opts.get(key).unwrap();
+            match value {
+                Opt::Flag {
+                    short_name,
+                    long_name,
+                    description,
+                } => {
+                    println!(
+                        "\t-{0:<1}, --{1:<21}{2}",
+                        short_name, long_name, description
+                    )
+                }
+                Opt::Value {
+                    short_name,
+                    long_name,
+                    description,
+                    default,
+                } => {
+                    println!(
+                        "\t-{0:<1} <val>, --{1:<15}{2}",
+                        short_name,
+                        format!("{} <val>", long_name),
+                        format!("{} Default is value '{}'.", description, default)
+                    )
+                }
+            }
+        }
     }
 }
 
@@ -111,41 +248,29 @@ mod tests {
     #[test]
     fn add_opts() {
         let mut p = Parser::new();
-        let opt = Opt::Flag {
-            short_name: "v".to_string(),
-            long_name: "version".to_string(),
-            description: "Print version".to_string(),
-        };
-        assert_eq!(p.short_opts.len(), 0);
+        let opt = opt_flag!("s", "some", "Some option");
+        assert_eq!(p.short_opts.len(), 2);
         p.add_opt(opt);
-        assert_eq!(p.short_opts.len(), 1);
+        assert_eq!(p.short_opts.len(), 3);
     }
     #[test]
     fn parse_version_opt() {
         let mut p = Parser::new();
-        let opt = Opt::Flag {
-            short_name: "v".to_string(),
-            long_name: "version".to_string(),
-            description: "Print version".to_string(),
-        };
+        let opt = opt_flag!("s", "some", "Some option");
         p.add_opt(opt);
-        let r = p.parse(vec!["-v".to_string()]);
+        let r = p.parse(vec!["-s".to_string()]);
         eprintln!("{:?}", r);
-        assert_eq!(r.get(&"version".to_string()), Some(&"".to_string()))
+        assert_eq!(r.opts.get(&"some".to_string()), Some(&"".to_string()))
     }
     #[test]
     fn parse_source_file_val_opt() {
         let mut p = Parser::new();
-        let opt = Opt::Value {
-            short_name: "s".to_string(),
-            long_name: "source-file".to_string(),
-            description: "Print version".to_string(),
-        };
+        let opt = opt_val!("s", "source-file", "Some option", "def");
         p.add_opt(opt);
         let r = p.parse(vec!["-s".to_string(), "LICENSE".to_string()]);
         eprintln!("{:?}", r);
         assert_eq!(
-            r.get(&"source-file".to_string()),
+            r.opts.get(&"source-file".to_string()),
             Some(&"LICENSE".to_string())
         )
     }
