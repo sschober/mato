d6da10e7e8f198f2a828d03a04d4c6ff06e8ca79
Sven Schober <sv3sch@gmail.com>
2025-05-28 20:21:11 +0200

    process/image_converter: refactor into walk and resolve_path functions

--- src/process/image_converter.rs
+++ src/process/image_converter.rs
@@ -15,38 +15,42 @@ pub struct ImageConverter<'a> {
 }
 
 impl ImageConverter<'_> {
-    fn process_images(&mut self, exp: Tree) -> Tree {
+    /// walks the tree until it find Tree::Image nodes, uses recursive descent
+    fn walk(&mut self, exp: Tree) -> Tree {
         match exp {
-            Tree::Document(dt, be) => Tree::Document(dt, Box::new(self.process_images(*be))),
-            Tree::Cat(b1, b2) => self.process_images(*b1).cat(self.process_images(*b2)),
-            Tree::Image(caption, path, size_spec) => {
-                let path = match *path {
-                    Tree::Literal(p) => {
-                        let mut resolved_path = p.clone();
-                        if !p.starts_with('/') {
-                            let parent_dir_path = crate::parent_dir(&self.config.source_file);
-                            resolved_path = parent_dir_path
-                                .join(p)
-                                .as_os_str()
-                                .to_str()
-                                .unwrap()
-                                .to_string();
-                        }
-                        m_dbg!("resolved path: {}", resolved_path);
-                        lit(&resolved_path)
-                    }
-                    _ => *path,
-                };
-                image(*caption, path, *size_spec)
-            }
+            Tree::Document(dt, be) => Tree::Document(dt, Box::new(self.walk(*be))),
+            Tree::Cat(b1, b2) => self.walk(*b1).cat(self.walk(*b2)),
+            Tree::Image(caption, path, size_spec) => self.resolve_path(*caption, *path, *size_spec),
             _ => exp,
         }
+    }
+
+    /// resolves path specs in image references using the source file making them absolute
+    fn resolve_path(&mut self, caption: Tree, path: Tree, size_spec: Tree) -> Tree {
+        let path = match path {
+            Tree::Literal(p) => {
+                let mut resolved_path = p.clone();
+                if !p.starts_with('/') {
+                    let parent_dir_path = crate::parent_dir(&self.config.source_file);
+                    resolved_path = parent_dir_path
+                        .join(p)
+                        .as_os_str()
+                        .to_str()
+                        .unwrap()
+                        .to_string();
+                }
+                m_dbg!("resolved path: {}", resolved_path);
+                lit(&resolved_path)
+            }
+            _ => path,
+        };
+        image(caption, path, size_spec)
     }
 }
 impl Process for ImageConverter<'_> {
     fn process(&mut self, exp: crate::syntax::Tree) -> crate::syntax::Tree {
         m_trc!("{:?}", self);
-        self.process_images(exp)
+        self.walk(exp)
     }
 }
 
